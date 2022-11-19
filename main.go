b5ebd3030fdb8b8953f30b508dabcfff5319f78a
mojafa <jafa@Jafas-MacBook-Pro.local>
2022-11-19 12:37:48 +0300

    added new URL Query param

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 // Define a home handler function which writes a byte slice containing
@@ -18,6 +20,19 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 // Add a showSnippet handler function.
 func showSnippet(w http.ResponseWriter, r *http.Request) {
+	//Extract th value of the id parameter from the query string and try to
+	// convert it to an integer using the strconv.Atoi() function. If it can't
+	// be converted to an integer, or the value is less than 1, we return a 404
+	// not found response.
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	//use the fmt.Printf() function to interpolate the id value with our
+	// response and write it to the http.ResponseWriter.
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 	w.Write([]byte("Display a specific snippet..."))
 }
 
@@ -29,22 +44,22 @@ func createSnippet(w http.ResponseWriter, r *http.Request) {
 	// then return from the function so that the subsequent code is not execute
 
 	if r.Method != "POST" {
-
 		// Use the Header().Set() method to add an 'Allow: POST' header to the
 		// response header map. The first parameter is the header name, and
 		// the second parameter is the header value.
+		//w.Header().Set("Allow", "POST")
 		w.Header().Set("Allow", "POST")
-
-		// w.WriteHeader(405)
-		// w.Write([]byte("Method Not Allowed"))
-
+		//suppresisng the date header content
+		//w.Header()["Date"] = nil
+		//w.WriteHeader(405)
+		//w.Write([]byte("Method Not Allowed"))
 		// Use the http.Error() function to send a 405 status code and "Method Allowed" string as the response body.
+
+		w.Header().Set("Content-Type", "application/json")
 		http.Error(w, "Method Not Allowed", 405)
 		return
 	}
-
 	w.Write([]byte("Create a new snippet..."))
-
 }
 
 func main() {
