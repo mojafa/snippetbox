0132b49aa8a3e27e18b3ef6dc75d493cc6b3778e
MoJafa <jmohsa@gmail.com>
2023-05-01 06:28:45 +0300

    added create and show requests

--- main.go
+++ main.go
@@ -8,10 +8,19 @@ import (
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from snippetbox"))
 }
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet..."))
+}
+
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new snippet..."))
+}
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
 
 	log.Println("Starting on server on port 4000")
 	err := http.ListenAndServe(":4000", mux)
