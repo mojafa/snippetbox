380800895586eb7fc0400730a9328a05387daa51
MoJafa <jmohsa@gmail.com>
2023-05-01 19:56:16 +0300

    dependency injection

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,25 +7,33 @@ import (
 	"os"
 	"path/filepath"
 )
-
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+}
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	flag.Parse()
 
+	// f, err := os.OpenFile("/tmp/info.log", os.O_RDWR|os.O_CREATE, 0666)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// defer f.Close()
 
-	f, err := os.OpenFile("/tmp/info.log",os.O_RDWR|os.O_CREATE, 0666)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer f.Close()
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime|log.LUTC)
 	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.LUTC|log.Lshortfile)
+
+	app := &application{
+		errorLog: errorLog,
+		infoLog:  infoLog,
+	}
 	//locally scoped servemux.
 	//This is a good practice to avoid polluting the global namespace.
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet", showSnippet)
-	mux.HandleFunc("/snippet/create", createSnippet)
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/snippet", app.showSnippet)
+	mux.HandleFunc("/snippet/create", app.createSnippet)
 
 	// Create a file server which serves files out of the "./ui/static" directory. // Note that the path given to the http.Dir function is relative to the project // directory root.
 	// fileServer := http.FileServer(http.Dir("./ui/static/"))
@@ -35,9 +43,9 @@ func main() {
 	//Use the mux.Handle() function to register the file server as the handler for // all URL paths that start with "/static/". For matching paths, we strip the // "/static" prefix before the request reaches the file server.
 
 	srv := &http.Server{
-		Addr: *addr,
+		Addr:     *addr,
 		ErrorLog: errorLog,
-		Handler: mux,
+		Handler:  mux,
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
