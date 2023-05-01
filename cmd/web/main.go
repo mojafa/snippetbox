53e1e0229ccdb208a63f9f28446e48a4eb1257c4
MoJafa <jmohsa@gmail.com>
2023-05-01 11:07:57 +0300

    project structure

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -17,11 +17,11 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 func showSnippet(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
-	if err != nil || id <1 {
-		http.NotFound(w,r)
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
 		return
 	}
-	fmt.Fprintf(w,"Display a specific snippet with ID %d...", id)
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
 
 func createSnippet(w http.ResponseWriter, r *http.Request) {
