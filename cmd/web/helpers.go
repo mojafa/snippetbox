33de09927c4a127d507edcb9652d45355dcca5e4
MoJafa <jmohsa@gmail.com>
2023-05-02 10:43:07 +0300

    testing the error handling

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -5,10 +5,11 @@ import (
 	"net/http"
 	"runtime/debug"
 )
+
 // the serverError() helper we use the debug.Stack() function to get a stack trace for the current goroutine and append it to the log messag
 func (app *application) serverError(w http.ResponseWriter, err error) {
 	trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
-	app.errorLog.Println(trace)
+	app.errorLog.Output(2, trace)
 
 	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 }
