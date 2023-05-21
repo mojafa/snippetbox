ce47603c1ac06a013c39ff6d5b0729703ffb8761
mojafa <jmohsa@gmail.com>
2023-05-21 11:36:27 +0300

    GETting from a DB in go, stmt then queryrow returns a pointer, init a pointer to a new zeroed struct, use rowscan to copy the values -> so cool

--- pkg/models/mysql/snippets.go
+++ pkg/models/mysql/snippets.go
@@ -27,7 +27,8 @@ func (m *SnippetModel) Insert(title, content, expires string) (int, error) {
 	if err != nil {
 		return 0, err
 	}
-
+	// The ID returned has the type int64, so we convert it to an int type
+	// before returning.
 	return int(id), nil
 }
 
@@ -35,13 +36,20 @@ func (m *SnippetModel) Insert(title, content, expires string) (int, error) {
 func (m *SnippetModel) Get(id int) (*models.Snippet, error) {
 	stmt := `SELECT id, title, content, created, expires FROM snippets
 	WHERE expires > UTC_TIMESTAMP() AND id = ?`
+
+	// Use the QueryRow() method on the connection pool to execute our
+	// SQL statement, passing in the untrusted id variable as the value for the
+	// placeholder parameter. This returns a pointer to a sql.Row object which 
+	// holds the result from the database.
 	row := m.DB.QueryRow(stmt, id)
 	// Initialize a pointer to a new zeroed Snippet struct.
 	s := &models.Snippet{}
 
 	// Use row.Scan() to copy the values from each field in sql.Row to the
 	// corresponding field in the Snippet struct. Notice that the arguments
-	// to row.Scan are *pointers* to the place you want to copy the data into, // and the number of arguments must be exactly the same as the number of // columns returned by your statement.
+	// to row.Scan are *pointers* to the place you want to copy the data into,
+	// and the number of arguments must be exactly the same as the number of
+	// columns returned by your statement.
 	err := row.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
 	if err != nil {
 		// If the query returns no rows, then row.Scan() will return a
